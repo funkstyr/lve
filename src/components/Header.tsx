b411395e42b54f537874d68e3468967d91db8e0d
Michael Funk <mfunk@brandsource.com>
2021-12-03 13:42:41 -0500

    feat: add about us page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 const Header = () => {
   return (
@@ -11,8 +11,8 @@ const Header = () => {
 //export default Header;
 
 /* This example requires Tailwind CSS v2.0+ */
-import { Fragment } from 'react';
-import { Popover, Transition } from '@headlessui/react';
+import { Fragment } from "react";
+import { Popover, Transition } from "@headlessui/react";
 import {
   ChartBarIcon,
   CursorClickIcon,
@@ -21,119 +21,125 @@ import {
   ShieldCheckIcon,
   ViewGridIcon,
   XIcon,
-} from '@heroicons/react/outline';
-import { ChevronDownIcon } from '@heroicons/react/solid';
+} from "@heroicons/react/outline";
+import { ChevronDownIcon } from "@heroicons/react/solid";
 
 const solutions = [
   {
-    name: 'Analytics',
+    name: "Analytics",
     description:
-      'Get a better understanding of where your traffic is coming from.',
-    href: '#',
+      "Get a better understanding of where your traffic is coming from.",
+    href: "#",
     icon: ChartBarIcon,
   },
   {
-    name: 'Engagement',
-    description: 'Speak directly to your customers in a more meaningful way.',
-    href: '#',
+    name: "Engagement",
+    description: "Speak directly to your customers in a more meaningful way.",
+    href: "#",
     icon: CursorClickIcon,
   },
   {
-    name: 'Security',
+    name: "Security",
     description: "Your customers' data will be safe and secure.",
-    href: '#',
+    href: "#",
     icon: ShieldCheckIcon,
   },
   {
-    name: 'Integrations',
+    name: "Integrations",
     description: "Connect with third-party tools that you're already using.",
-    href: '#',
+    href: "#",
     icon: ViewGridIcon,
   },
   {
-    name: 'Automations',
+    name: "Automations",
     description:
-      'Build strategic funnels that will drive your customers to convert',
-    href: '#',
+      "Build strategic funnels that will drive your customers to convert",
+    href: "#",
     icon: RefreshIcon,
   },
 ];
 
 function classNames(...classes: string[]) {
-  return classes.filter(Boolean).join(' ');
+  return classes.filter(Boolean).join(" ");
 }
 
 export default function Example() {
   return (
-    <Popover className="relative bg-white">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6">
-        <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
-          <div className="flex justify-start lg:w-0 lg:flex-1">
-            <Link href="/">
+    <Popover className='relative bg-white'>
+      <div className='max-w-7xl mx-auto px-4 sm:px-6'>
+        <div className='flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10'>
+          <div className='flex justify-start lg:w-0 lg:flex-1'>
+            <Link href='/'>
               <a>
-                <span className="sr-only">LVE</span>
+                <span className='sr-only'>LVE</span>
                 <img
-                  className="h-8 w-auto sm:h-10"
-                  src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-                  alt=""
+                  className='h-8 w-auto sm:h-10'
+                  src='https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg'
+                  alt=''
                 />
               </a>
             </Link>
           </div>
 
-          <div className="-mr-2 -my-2 md:hidden">
-            <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-              <span className="sr-only">Open menu</span>
-              <MenuIcon className="h-6 w-6" aria-hidden="true" />
+          <div className='-mr-2 -my-2 md:hidden'>
+            <Popover.Button className='bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500'>
+              <span className='sr-only'>Open menu</span>
+              <MenuIcon className='h-6 w-6' aria-hidden='true' />
             </Popover.Button>
           </div>
 
-          <Popover.Group as="nav" className="hidden md:flex space-x-10">
-            <Popover className="relative">
+          <Popover.Group as='nav' className='hidden md:flex space-x-10'>
+            <Popover className='relative'>
               {({ open }) => (
                 <>
                   <Popover.Button
                     className={classNames(
-                      open ? 'text-gray-900' : 'text-gray-500',
-                      'group bg-white rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+                      open ? "text-gray-900" : "text-gray-500",
+                      "group bg-white rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                     )}
                   >
                     <span>Who We Are</span>
                     <ChevronDownIcon
                       className={classNames(
-                        open ? 'text-gray-600' : 'text-gray-400',
-                        'ml-2 h-5 w-5 group-hover:text-gray-500'
+                        open ? "text-gray-600" : "text-gray-400",
+                        "ml-2 h-5 w-5 group-hover:text-gray-500"
                       )}
-                      aria-hidden="true"
+                      aria-hidden='true'
                     />
                   </Popover.Button>
 
                   <Transition
                     as={Fragment}
-                    enter="transition ease-out duration-200"
-                    enterFrom="opacity-0 translate-y-1"
-                    enterTo="opacity-100 translate-y-0"
-                    leave="transition ease-in duration-150"
-                    leaveFrom="opacity-100 translate-y-0"
-                    leaveTo="opacity-0 translate-y-1"
+                    enter='transition ease-out duration-200'
+                    enterFrom='opacity-0 translate-y-1'
+                    enterTo='opacity-100 translate-y-0'
+                    leave='transition ease-in duration-150'
+                    leaveFrom='opacity-100 translate-y-0'
+                    leaveTo='opacity-0 translate-y-1'
                   >
-                    <Popover.Panel className="absolute z-10 -ml-4 mt-3 transform px-2 w-screen max-w-md sm:px-0 lg:ml-0 lg:left-1/2 lg:-translate-x-1/2">
-                      <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
-                        <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
-                          <Link href="/leadership">
-                            <a className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">
+                    <Popover.Panel className='absolute z-10 -ml-4 mt-3 transform px-2 w-screen max-w-md sm:px-0 lg:ml-0 lg:left-1/2 lg:-translate-x-1/2'>
+                      <div className='rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden'>
+                        <div className='relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8'>
+                          <Link href='/about-us'>
+                            <a className='-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50'>
+                              About Us
+                            </a>
+                          </Link>
+
+                          <Link href='/leadership'>
+                            <a className='-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50'>
                               Leadership
                             </a>
                           </Link>
 
-                          <Link href="/management-team">
-                            <a className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">
+                          <Link href='/management-team'>
+                            <a className='-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50'>
                               Management Team
                             </a>
                           </Link>
 
-                          <Link href="/evolution">
-                            <a className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">
+                          <Link href='/evolution'>
+                            <a className='-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50'>
                               LVE EVoLution
                             </a>
                           </Link>
@@ -145,26 +151,26 @@ export default function Example() {
               )}
             </Popover>
 
-            <Link href="/careers">
-              <a className="text-base font-medium text-gray-500 hover:text-gray-900">
+            <Link href='/careers'>
+              <a className='text-base font-medium text-gray-500 hover:text-gray-900'>
                 Careers
               </a>
             </Link>
 
-            <Link href="/community-page">
-              <a className="text-base font-medium text-gray-500 hover:text-gray-900">
+            <Link href='/community-page'>
+              <a className='text-base font-medium text-gray-500 hover:text-gray-900'>
                 Community
               </a>
             </Link>
 
-            <Link href="/food-info">
-              <a className="text-base font-medium text-gray-500 hover:text-gray-900">
+            <Link href='/food-info'>
+              <a className='text-base font-medium text-gray-500 hover:text-gray-900'>
                 Our Food
               </a>
             </Link>
 
-            <Link href="/contact">
-              <a className="text-base font-medium text-gray-500 hover:text-gray-900">
+            <Link href='/contact'>
+              <a className='text-base font-medium text-gray-500 hover:text-gray-900'>
                 Contact
               </a>
             </Link>
@@ -174,47 +180,47 @@ export default function Example() {
 
       <Transition
         as={Fragment}
-        enter="duration-200 ease-out"
-        enterFrom="opacity-0 scale-95"
-        enterTo="opacity-100 scale-100"
-        leave="duration-100 ease-in"
-        leaveFrom="opacity-100 scale-100"
-        leaveTo="opacity-0 scale-95"
+        enter='duration-200 ease-out'
+        enterFrom='opacity-0 scale-95'
+        enterTo='opacity-100 scale-100'
+        leave='duration-100 ease-in'
+        leaveFrom='opacity-100 scale-100'
+        leaveTo='opacity-0 scale-95'
       >
         <Popover.Panel
           focus
-          className="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
+          className='absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden'
         >
-          <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
-            <div className="pt-5 pb-6 px-5">
-              <div className="flex items-center justify-between">
+          <div className='rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50'>
+            <div className='pt-5 pb-6 px-5'>
+              <div className='flex items-center justify-between'>
                 <div>
                   <img
-                    className="h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-                    alt="Workflow"
+                    className='h-8 w-auto'
+                    src='https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg'
+                    alt='Workflow'
                   />
                 </div>
-                <div className="-mr-2">
-                  <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-                    <span className="sr-only">Close menu</span>
-                    <XIcon className="h-6 w-6" aria-hidden="true" />
+                <div className='-mr-2'>
+                  <Popover.Button className='bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500'>
+                    <span className='sr-only'>Close menu</span>
+                    <XIcon className='h-6 w-6' aria-hidden='true' />
                   </Popover.Button>
                 </div>
               </div>
-              <div className="mt-6">
-                <nav className="grid gap-y-8">
+              <div className='mt-6'>
+                <nav className='grid gap-y-8'>
                   {solutions.map((item) => (
                     <a
                       key={item.name}
                       href={item.href}
-                      className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50"
+                      className='-m-3 p-3 flex items-center rounded-md hover:bg-gray-50'
                     >
                       <item.icon
-                        className="flex-shrink-0 h-6 w-6 text-indigo-600"
-                        aria-hidden="true"
+                        className='flex-shrink-0 h-6 w-6 text-indigo-600'
+                        aria-hidden='true'
                       />
-                      <span className="ml-3 text-base font-medium text-gray-900">
+                      <span className='ml-3 text-base font-medium text-gray-900'>
                         {item.name}
                       </span>
                     </a>
@@ -222,28 +228,28 @@ export default function Example() {
                 </nav>
               </div>
             </div>
-            <div className="py-6 px-5 space-y-6">
-              <div className="grid grid-cols-2 gap-y-4 gap-x-8">
-                <Link href="/careers">
-                  <a className="text-base font-medium text-gray-500 hover:text-gray-900">
+            <div className='py-6 px-5 space-y-6'>
+              <div className='grid grid-cols-2 gap-y-4 gap-x-8'>
+                <Link href='/careers'>
+                  <a className='text-base font-medium text-gray-500 hover:text-gray-900'>
                     Careers
                   </a>
                 </Link>
 
-                <Link href="/community-page">
-                  <a className="text-base font-medium text-gray-500 hover:text-gray-900">
+                <Link href='/community-page'>
+                  <a className='text-base font-medium text-gray-500 hover:text-gray-900'>
                     Community
                   </a>
                 </Link>
 
-                <Link href="/food-info">
-                  <a className="text-base font-medium text-gray-500 hover:text-gray-900">
+                <Link href='/food-info'>
+                  <a className='text-base font-medium text-gray-500 hover:text-gray-900'>
                     Our Food
                   </a>
                 </Link>
 
-                <Link href="/contact">
-                  <a className="text-base font-medium text-gray-500 hover:text-gray-900">
+                <Link href='/contact'>
+                  <a className='text-base font-medium text-gray-500 hover:text-gray-900'>
                     Contact
                   </a>
                 </Link>
