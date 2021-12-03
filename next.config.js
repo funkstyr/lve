af6ad432ac7095674bf4bdf6e284649c94a0d6b5
funkstyr <30911142+funkstyr@users.noreply.github.com>
2021-12-02 20:16:09 -0500

    ref: reorg under src dir

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+  },
+})
+
 /** @type {import('next').NextConfig} */
-module.exports = {
+const config = {
   reactStrictMode: true,
+  swcMinify: true,
+
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
 }
+
+module.exports = withMDX(config);
